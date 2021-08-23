edfe66654ea2000a12a90fe48cc3be970cd24faa
Andrew Harvey <andrew@mootpointer.com>
2021-08-23 15:46:57 +1000

    Pass sshd relevant ENV variables for running rails

--- startup.sh
+++ startup.sh
@@ -19,7 +19,7 @@ if [ -n "$SKIP_MIGRATIONS" ]
 fi
 
 echo "Starting SSH Server"
-/usr/sbin/sshd
+/usr/sbin/sshd -o "SetEnv=RAILS_ENV=\"$RAILS_ENV\" DATABASE_URL=\"$DATABASE_URL\" SECRET_KEY_BASE=stubbed"
 
 echo "## Migrations complete. Starting app."
 # Start App
